b4297c949be3e4df06eb0b94a6632979e27e5234
mattzippyking <137015975+mattzippyking@users.noreply.github.com>
2023-06-21 13:06:21 +0800

    Update IDoctor.cs

--- DoctorsSurgery/Entities/IDoctor.cs
+++ DoctorsSurgery/Entities/IDoctor.cs
@@ -4,9 +4,9 @@
     public interface IDoctor
     {
         /// <summary> Doctor's name </summary>
-        string Name { get; set; }
+        string Name { get; set; } 
         
         /// <summary> Unique identification number in Guid format </summary>
-        Guid Id { get; set; }
+        Guid Id { get; set; } = Guid.NewGuid();
     }
 }
