2cc28f7d3bcce62d3e573cce74376ed009387d0e
mattzippyking <137015975+mattzippyking@users.noreply.github.com>
2023-06-21 13:08:36 +0800

    Update Appointment.cs

--- DoctorsSurgery/Entities/Appointment.cs
+++ DoctorsSurgery/Entities/Appointment.cs
@@ -2,9 +2,15 @@
 {
     public class Appointment : IAppointment
     {
+        /// <summary> Unique identification number for the appointment in Guid format </summary>
         public Guid Id { get; set; }
+        
+        /// <summary> Reference to the identification number for the patient in Guid format </summary>
         public Guid PatientId { get; set; }
+        
+        /// <summary> Reference to the identification number for the slot in Guid format </summary>
         public Guid SlotId { get; set; }
+        
         public DateTime CreatedDate { get; set; }
     }
 }
